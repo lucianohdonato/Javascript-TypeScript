953cf9f8e362475720b6887e2c40fcaaae259422
Turma Generation <luciano.h.donato@gmail.com>
2019-11-29 14:14:54 -0300

    iniciando com dom

---
+++ 11-29/main.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var email = document.getElementById("email");
+    var senha = document.getElementById("password");
+    if (email.value == "") {
+        alert("Informar email")
+        senha.value = null;
+    } else if (senha.value == "") {
+        alert("Informar senha")
+    } else if (senha.value == "senha") {
+        window.location.replace("https://g1.globo.com/");
+    } else {
+        alert("Senha inválida");
+        senha.value = null;
+    }
+}
